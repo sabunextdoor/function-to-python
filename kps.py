c94c088ffd7d1ede4dfe1635c2545fad34cf01f4
sabunextdoor <sabuthwala@gmail.com>
2024-11-11 19:16:11 +0200

    function in python

---
+++ kps.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+
+x=int(input("enter x"))
+y=int(input("enter y"))
+print(add(x,y))
+print(add(x,y))
